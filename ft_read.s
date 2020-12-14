6212719ac16e1b904004b704625262abaddc1199
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2020-12-14 16:56:03 +0900

    整理しました

--- ft_read.s
+++ ft_read.s
@@ -1,17 +1,27 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_read.s                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/12/14 16:42:57 by tmurakam          #+#    #+#              #
+#    Updated: 2020/12/14 16:55:24 by tmurakam         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 section .text
 global ft_read
 extern __errno_location
 
-ft_read:	mov		rax, 0 ; sytem call id - read : 0
+ft_read:	mov		rax		, 0		; system call id - read : 0
 			syscall
-			cmp		rax, 0
-			jl		.error
-			ret
-.error:		push	rax
-			call __errno_location wrt ..plt
-			mov		rdx,	rax
-			pop		rax
-			imul	rax,	-1
-			mov		[rdx],	rax
-			mov		rax,	-1
-			ret
+			cmp		rax		, 0		; compare 0 and syscall result(rax) 
+			jl		.error			; if rax is less than 0, got .error
+			ret						; return normal case
+.error:		imul	rax		, -1	; result is the errno * (-1),
+			move	rdx		, rax	; keep error code rdx,
+			call	__errno_location wrt ..plt ; result is call is pointer of errno, 
+			mov		[rax]	, rdx	; set errno
+			mov		rax		, -1	; set error code
+			ret						; return error case
--- ft_write.s
+++ ft_write.s
@@ -1,17 +1,27 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_write.s                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/12/14 16:42:28 by tmurakam          #+#    #+#              #
+#    Updated: 2020/12/14 16:55:03 by tmurakam         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 section .text
 global ft_write
 extern __errno_location
 
-ft_write:	mov		rax, 1 ; sytem call id - write : 1
+ft_write:	mov		rax		, 1 	; system call id - write : 1
 			syscall
-			cmp		rax, 0
-			jl		.error
-			ret
-.error:		push	rax
-			call __errno_location wrt ..plt
-			mov		rdx,	rax
-			pop		rax
-			imul	rax,	-1
-			mov		[rdx],	rax
-			mov		rax,	-1
-			ret
+			cmp		rax		, 0		; compare 0 and syscall result(rax) 
+			jl		.error			; if rax is less than 0, got .error
+			ret						; return normal case
+.error:		imul	rax		, -1	; result is the errno * (-1),
+			move	rdx		, rax	; keep error code rdx,
+			call	__errno_location wrt ..plt ; result is call is pointer of errno, 
+			mov		[rax]	, rdx	; set errno
+			mov		rax		, -1	; set error code
+			ret						; return error case
