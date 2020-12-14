2114be81abbb66996282bb05863766a0ab3df3a3
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2020-12-14 16:59:19 +0900

    spell miss in mov as move

--- ft_read.s
+++ ft_read.s
@@ -6,7 +6,7 @@
 #    By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/14 16:42:57 by tmurakam          #+#    #+#              #
-#    Updated: 2020/12/14 16:55:24 by tmurakam         ###   ########.fr        #
+#    Updated: 2020/12/14 16:58:48 by tmurakam         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,7 +20,7 @@ ft_read:	mov		rax		, 0		; system call id - read : 0
 			jl		.error			; if rax is less than 0, got .error
 			ret						; return normal case
 .error:		imul	rax		, -1	; result is the errno * (-1),
-			move	rdx		, rax	; keep error code rdx,
+			mov		rdx		, rax	; keep error code rdx,
 			call	__errno_location wrt ..plt ; result is call is pointer of errno, 
 			mov		[rax]	, rdx	; set errno
 			mov		rax		, -1	; set error code
