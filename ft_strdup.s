cdc0b9dae9b4b053db2953cec7cc18c2cec77365
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2020-12-14 18:06:34 +0900

    aa

--- ft_strdup.s
+++ ft_strdup.s
@@ -6,7 +6,7 @@
 #    By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/14 16:42:40 by tmurakam          #+#    #+#              #
-#    Updated: 2020/12/14 16:42:42 by tmurakam         ###   ########.fr        #
+#    Updated: 2020/12/14 18:05:10 by tmurakam         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,14 +17,14 @@ extern ft_strlen
 extern ft_strcpy
 extern __errno_location
 
-ft_strdup:	push	rdi			; src -> stack
-			call	ft_strlen	; rax is the len of src(rdi)
-			mov		rdi, rax	; preper rdi (malloc 1st arg)
-			inc		rdi			; malloc size = strlen + 1
+ft_strdup:	push	rdi				; src -> stack
+			call	ft_strlen wrt ..plt	; rax is the len of src(rdi) "wrt ..plt" is not necessary?
+			mov		rdi		, rax	; preper rdi (malloc 1st arg)
+			inc		rdi				; malloc size = strlen + 1
 			call	malloc wrt ..plt
-			cmp		rax, 0		; if fail
-			jle		.end		; goto end
-			mov		rdi, rax	; malloc rax is the 1st arg of strcpy
-			pop		rsi			; src is the 2nd arg of strcpy
-			call	ft_strcpy
+			cmp		rax		, 0		; if fail
+			jle		.end			; goto end
+			mov		rdi		, rax	; malloc rax is the 1st arg of strcpy
+			pop		rsi				; src is the 2nd arg of strcpy
+			call	ft_strcpy wrt ..plt	
 .end:		ret
--- ft_write.s
+++ ft_write.s
@@ -6,7 +6,7 @@
 #    By: tmurakam <tmurakam@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/14 16:42:28 by tmurakam          #+#    #+#              #
-#    Updated: 2020/12/14 16:58:41 by tmurakam         ###   ########.fr        #
+#    Updated: 2020/12/14 18:05:53 by tmurakam         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,7 +20,7 @@ ft_write:	mov		rax		, 1 	; system call id - write : 1
 			jl		.error			; if rax is less than 0, got .error
 			ret						; return normal case
 .error:		imul	rax		, -1	; result is the errno * (-1),
-			mov		rdx		, rax	; keep error code rdx,
+			mov		rdx		, rax	; keep error code rdx into rdx
 			call	__errno_location wrt ..plt ; result is call is pointer of errno, 
 			mov		[rax]	, rdx	; set errno
 			mov		rax		, -1	; set error code
